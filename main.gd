d282909eb53008cc5ec5977eac5ea6f48378f829
DMMtm <DMM.trademark@gmail.com>
2025-08-07 17:31:57 -0500

    Gonna go ahead and drop this here

---
+++ main.gd
@@ -0,0 +1,11 @@
+'''
+Main.tscn
+
+For this project's case, all this file does is set up the test battle environment.
+'''
+extends Node
+
+var battle_scene = preload("res://scene/battle/Battle.tscn");
+
+func _ready():
+	print("TODO: define main function details");
